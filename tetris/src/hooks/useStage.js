86a8ed0cef849fa5361a14d4dfa73cc7bab81abf
Gillom McNeil <43531452+Gillyweed@users.noreply.github.com>
2022-01-12 22:48:03 -0400

    added scores, not working yet

--- tetris/src/hooks/useStage.js
+++ tetris/src/hooks/useStage.js
@@ -3,8 +3,26 @@ import { createStage } from '../gameHelpers';
 
 export const useStage = (player, resetPlayer) => {
     const [stage, setStage] = useState(createStage());
+    const [rowsCleared, setRowsCleared] = useState(0);
 
     useEffect(() => {
+        setRowsCleared(0);
+
+        const sweepRows = newStage =>
+            // acumulator?
+            newStage.reduce((ack, row) => {
+                // if we don't find any cells with the value of 0
+                if (row.findIndex(cell => cell[0] === 0) === -1) {
+                    // sweep row
+                    setRowsCleared(prev => prev + 1);
+                    // add empty rows at the beginning of accumulator (top of screen)
+                    ack.unshift(new Array(newStage[0].length).fill([0, 'clear']));
+                    return ack;
+                }
+                ack.push(row);
+                return ack;
+            }, [])
+
         const updateStage = prevStage => {
             // first flush the stage
             const newStage = prevStage.map(row =>
@@ -25,6 +43,8 @@ export const useStage = (player, resetPlayer) => {
             // Check if we have collided
             if (player.collided) {
                 resetPlayer();
+                // clear any rows that may be completed
+                return sweepRows(newStage);
             }
 
             return newStage;
@@ -34,5 +54,5 @@ export const useStage = (player, resetPlayer) => {
 
     }, [player, resetPlayer]);
 
-    return [stage, setStage];
+    return [stage, setStage, rowsCleared];
 };
